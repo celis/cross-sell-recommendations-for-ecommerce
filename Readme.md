95b1dc212ba46c820b6b303463580d960dacffac
celis <acelisalas@gmail.com>
2020-04-15 21:29:26 +0200

    update readme, update main script

--- Readme.md
+++ Readme.md
@@ -1,1 +1,59 @@
 # Cross-sell recommendations for E-commerce
+
+## A project submitted to the AWS Marketplace Developer Challenge
+#### website: https://awsmarketplaceml.devpost.com
+
+
+This project can run locally. It fetches transactional data from any Woocommerce Shop using the Woocommerce Rest API, then moves the data to an S3 bucket. The AWS Sagemaker Batch Transform Job retrieves this data from the S3 bucket and uses it to train a [Mphasis-Mphasis-HyperGraf-Market-Basket-Analysis](https://aws.amazon.com/marketplace/pp/Mphasis-Mphasis-HyperGraf-Market-Basket-Analysis/prodview-y6z3fk7gvudgs) model from the AWS Marketplace.   The model outputs product recommendations based on patterns extracted from previous customer purchases. These recommendations can be used in the online shop for cross-selling purposes.  In order to integrate these recommendations to the online shop, we use the Woocommerce Rest API to upload the recommendations to the MySql database of WordPress.
+
+After running the full notebook, we will have populated the 'cross_sell_ids' field of our MySQL database (which is empty by default).  Woocommerce provides several options to visualize these product recommendations as sliders on Product or Checkout pages.
+
+## Usage 
+
+Create a virtual enviroment using 
+
+    conda create -n cross-seller-env pip
+    
+Install dependencies with 
+
+    pip install -r requirements.txt
+    
+Activate the environment 
+
+    conda activate cross-seller-env
+    
+Create configuration variables file 'configs/config.json', following the template provided (type, example):
+
+```json
+{
+   "aws_keys":{
+      "access_key": (str),
+      "secret_key": (str)
+   },
+   "batch_transform_job":{
+      "role_name": (str, "AmazonSageMaker-ExecutionRole-..."),
+      "model_package_arn": (str, "arn:aws:sagemaker:eu-central-1:...",
+      "instance_type": (str, "ml.m5.large"),
+      "instance_count": (int, 1)
+   },
+   "s3":{
+      "region_name": (str, "eu-central-1"),
+      "bucket": (str , "mybucket"),
+      "input_file": (str, "input_data.csv")
+   },
+   "woocommerce":{
+      "url": (str),
+      "consumer_key": (str),
+      "consumer_secret": (str)
+   }
+}       
+```
+
+
+## Running Batch Transform Job
+
+To run a batch transform job you can use the CLI provided
+
+    python update_recommendations.py -i "data/input_data.csv" -o "data/output_data.csv"
+    
+Alternatively, you can use the jupyter notebook provided 'aws_marketplace_cross_sell_recommendations.ipynb'.  
--- update_recommendations.py
+++ update_recommendations.py
@@ -1,11 +1,100 @@
 from src.configuration import Configuration
 from src.services import S3, Woocommerce
+import logging
+import os
+import argparse
+import boto3
+from sagemaker import ModelPackage
+import sagemaker as sagemaker
+
+
+def cli():
+    """
+    Simply run this script with '--help' to see all options.
+    """
+    desc = "Pipeline to train a AWS Marketplace model on Woocommerce data and upload cross-sell product recommendations"
+
+    logging.basicConfig(
+        format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
+    )
+
+    parser = argparse.ArgumentParser(description=desc)
+
+    parser.add_argument("-i", "--input", help="input path", dest="input", required=True)
+
+    parser.add_argument(
+        "-o", "--output", help="output_path", dest="output", required=True
+    )
+
+    args = parser.parse_args()
+
+    logging.info("configure services")
+    config = Configuration("configs/config.json").parameters
+
+    s3 = S3(
+        region_name=config["s3"]["region_name"],
+        access_key=config["aws_keys"]["access_key"],
+        secret_key=config["aws_keys"]["secret_key"],
+        bucket=config["s3"]["bucket"],
+    )
+
+    woocommerce = Woocommerce(
+        url=config["woocommerce"]["url"],
+        consumer_key=config["woocommerce"]["consumer_key"],
+        consumer_secret=config["woocommerce"]["consumer_secret"],
+    )
+
+    logging.info("download data from Woocommerce")
+    woocommerce.download_data(args.input)
+
+    logging.info("upload data to S3")
+    s3.upload(args.input, "input_data.csv")
+
+    logging.info("configure Batch Transform Job")
+    model_package_arn = config["batch_transform_job"]["model_package_arn"]
+
+    iam = boto3.client("iam")
+    role = iam.get_role(RoleName=config["batch_transform_job"]["role_name"])["Role"][
+        "Arn"
+    ]
+
+    sagemaker_session = sagemaker.Session()
+
+    model = ModelPackage(
+        model_package_arn=model_package_arn,
+        role=role,
+        sagemaker_session=sagemaker_session,
+    )
+
+    output_path = "s3://" + os.path.join(config["s3"]["bucket"], "output")
+
+    transformer = model.transformer(
+        instance_count=config["batch_transform_job"]["instance_count"],
+        instance_type=config["batch_transform_job"]["instance_type"],
+        output_path=output_path,
+    )
+
+    input_path = "s3://" + os.path.join(
+        config["s3"]["bucket"], config["s3"]["input_file"]
+    )
+
+    transformer.transform(input_path, content_type="text/csv")
+
+    logging.info('batch transform job starting')
+    transformer.wait()
+
+    logging.info("downloading recommendations from S3")
+    bucket_folder = transformer.output_path.rsplit("/")[3]
+    key = os.path.join(
+        bucket_folder, config["s3"]["input_file"].split("/")[-1] + ".out"
+    )
+
+    s3.download(key, args.output)
+
+    logging.info("Push recommendations to Woocommerce")
+    woocommerce.upload_recommendations(args.output)
 
 
 if __name__ == "__main__":
 
-    config = Configuration("configs/config.json").parameters
-
-    s3 = S3()
-
-    woocommerce = Woocommerce()
+    cli()
